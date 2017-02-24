5ee0a188d4a15be25fe1e9ecca0dfdc8e2366b4a
ludojmj <ludovic.villejarno@gmail.com>
2017-02-24 17:18:44 +0100

    Simple return in GenericExceptionHandler when unmanaged exception is raised

--- BoilerWebApiCore/Shared/GenericExceptionHandler.cs
+++ BoilerWebApiCore/Shared/GenericExceptionHandler.cs
@@ -1,26 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Hosting;
 using BoilerWebApiCore.Models;
 
 namespace BoilerWebApiCore.Shared
 {
     public class GenericExceptionHandler : IExceptionFilter
     {
-        private readonly IHostingEnvironment _env;
-
-        public GenericExceptionHandler(IHostingEnvironment env)
-        {
-            _env = env;
-        }
-
         public void OnException(ExceptionContext context)
         {
             var businessException = context.Exception as BusinessException;
             if (businessException == null)
             {   // Not a managed BusinessException
                 // ==> Delegating logging to AspNet: UseExceptionHandler/ErrorController
-                throw context.Exception;
+                return;
             }
 
             var businessError = new GenericError { Message = context.Exception.Message };
