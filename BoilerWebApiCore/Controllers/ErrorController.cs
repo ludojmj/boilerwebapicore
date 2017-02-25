8afbd4e4edf359d2729438be1e1d32a38df75185
ludojmj <ludovic.villejarno@gmail.com>
2017-02-25 15:29:21 +0100

    Simplification

--- BoilerWebApiCore/Controllers/ErrorController.cs
+++ BoilerWebApiCore/Controllers/ErrorController.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Diagnostics;
 using BoilerWebApiCore.Models;
-using Microsoft.AspNetCore.Hosting;
 
 namespace BoilerWebApiCore.Controllers
 {
@@ -11,13 +9,6 @@ namespace BoilerWebApiCore.Controllers
     [Route("api/[controller]")]
     public class ErrorController : Controller
     {
-        private readonly IHostingEnvironment _env;
-
-        public ErrorController(IHostingEnvironment env)
-        {
-            _env = env;
-        }
-
         // Swagger needs an explicit HttpMethod
         // And UseExceptionHandler middleware requires HttpGet *AND* HttpPost according to source controllers
         // (and probably HttpPut, HttpDelete, ... But since there are not used in other controllers, we won't set them) 
@@ -25,17 +16,8 @@ namespace BoilerWebApiCore.Controllers
         [HttpPost]
         public IActionResult Get()
         {
-            dynamic error = new GenericError
-            {   // Default: We won't share any information publicly
-                Message = "An error occured. Please try again later."
-            };
-
-            if (_env.IsDevelopment())
-            {
-                // Development: We share detailed exception information publicly
-                error = HttpContext.Features.Get<IExceptionHandlerFeature>().Error;
-            }
-
+            // We won't share any information publicly
+            var error = new GenericError { Message = "An error occured. Please try again later." };
             return new JsonResult(error) { StatusCode = 409 };
         }
     }
